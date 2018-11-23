f1c664ec38a52ea84fd8265a7ac41a761e241bab
Daniel Sanchez <jesusdanielsar@gmail.com>
2018-11-22 21:33:38 -0500

    coolvetica
    coolvetica

--- app/src/main/java/com/gammadelta/gambitos/IngresarActivity.java
+++ app/src/main/java/com/gammadelta/gambitos/IngresarActivity.java
@@ -1,13 +1,25 @@
 package com.gammadelta.gambitos;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class IngresarActivity extends AppCompatActivity {
+
+    private TextView texto;
+    private Typeface coolvetica;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingresar);
+
+        String fuente1 = "fuentes/coolvetica.ttf";
+        this.coolvetica = Typeface.createFromAsset(getAssets(),fuente1);
+
+        texto = (TextView) findViewById(R.id.guambitos);
+        texto.setTypeface(coolvetica);
+
     }
 }
