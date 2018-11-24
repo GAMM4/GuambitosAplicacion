129794332b0c3fadb0b0ae43c93a1f1a6da621e4
Gamma <lauvandel@hotmail.com>
2018-11-24 17:15:38 -0500

    Creacion activitys de registro médico, padre e hijo de padte

---
+++ app/src/main/java/com/gammadelta/gambitos/InicioMedicoActivity.java
@@ -0,0 +1,13 @@
+package com.gammadelta.gambitos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InicioMedicoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_medico);
+    }
+}
---
+++ app/src/main/java/com/gammadelta/gambitos/MedicoPadreActivity.java
@@ -0,0 +1,13 @@
+package com.gammadelta.gambitos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MedicoPadreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medico_padre);
+    }
+}
