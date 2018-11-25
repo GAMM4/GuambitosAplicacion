8959e37bda899dcfc99b8d5d7758e78ddb579d95
Daniel Sanchez <jesusdanielsar@gmail.com>
2018-11-25 18:37:25 -0500

    Creado hijos_padre y hijo_menu

---
+++ app/src/main/java/com/gammadelta/gambitos/HijoMenuActivity.java
@@ -0,0 +1,13 @@
+package com.gammadelta.gambitos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HijoMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hijo_menu);
+    }
+}
